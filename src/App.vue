43f7ed7b667f2393c1271bf970ce9af843eebff7
lgonzalez-3420 <little.red.cloud@gmail.com>
2024-01-28 17:58:48 -0600

    Generate ID

--- src/App.vue
+++ src/App.vue
@@ -42,9 +42,15 @@
   //handle transaction submitted
   const handleTransactionSubmitted = (transactionData) => {
     transactions.value.push({
+      id: generateUniqueId(),
       text: transactionData.text,
       amount: transactionData.amount,
     })
+  }
+
+  //generate unique id
+  const generateUniqueId = () => {
+    return Math.floor(Math.random() * 10000000)
   }
 
 
