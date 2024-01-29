9445c0c3221fbc94e59c707f6ad57073906cbae3
lgonzalez-3420 <little.red.cloud@gmail.com>
2024-01-28 21:33:23 -0600

    Added toastification

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
   import AddTransaction from './components/AddTransaction.vue';
   import TransactionList from './components/TransactionList.vue';
   import {ref, computed, onMounted} from 'vue'
-
+  import { useToast } from 'vue-toastification'
   const transactions = ref([])
+  const toast = useToast()
 
   // const transactions = ref([
   //   {id: 1, text: 'Paycheck', amount: 699.99},
@@ -50,6 +51,7 @@
     })
 
     saveTransactionToLocalStorage()
+    toast.success('Transaction Added')
   }
 
   //generate unique id
@@ -61,6 +63,7 @@
   const handleTransactionDeleted = (id) => {
     transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
     saveTransactionToLocalStorage()
+    toast.success('Transaction Deleted')
   }
 
   //save to local storage
